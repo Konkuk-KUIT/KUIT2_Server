73c71a81f0b20f92a11ee1985d328aeb1daa6d6f
beomjun <bzzun0609@github.com>
2023-11-25 22:38:25 +0900

    feat: simple mistake

---
+++ src/main/java/kuit2/server/controller/CartController.java
@@ -0,0 +1,37 @@
+//package kuit2.server.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PatchMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@Slf4j
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/cart")
+//public class CartController {
+//
+//    /**
+//     * 장바구니 조회
+//     */
+//    @GetMapping
+//
+//    /**
+//     * 장바구니 수정
+//     */
+//    @PatchMapping
+//
+//    /**
+//     * 장바구니 추가
+//     */
+//    @PostMapping
+//
+//    /**
+//     * 장바구니 삭제
+//     */
+//    @DeleteMapping
+//}
