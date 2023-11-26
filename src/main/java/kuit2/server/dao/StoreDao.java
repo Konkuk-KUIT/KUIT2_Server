a1315bc12a500681bb61243672ee4bd42dd52047
lisiant <wkdxod321@gmail.com>
2023-11-26 15:43:27 +0900

    feat: add StoreDao

---
+++ src/main/java/kuit2/server/dao/StoreDao.java
@@ -0,0 +1,33 @@
+package kuit2.server.dao;
+
+import kuit2.server.dto.store.PostStoreRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Slf4j
+@Repository
+public class StoreDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public StoreDao(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    public long createStore(PostStoreRequest postStoreRequest){
+        String sql = "insert into store(name, category, address, phone, content) VALUES (:name, :category, :address, :phone, :content)";
+        SqlParameterSource param = new BeanPropertySqlParameterSource(postStoreRequest);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(sql, param);
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+
+    }
+}
