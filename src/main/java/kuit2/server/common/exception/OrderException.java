d792079919ac72f7af8211edf77b73e8a89da7ec
lee-june-young <uhiccup@gmail.com>
2023-11-30 16:44:11 +0900

    feat: 주문하기 api 틀

--- src/main/java/kuit2/server/common/exception/OrderException.java
+++ src/main/java/kuit2/server/common/exception/OrderException.java
@@ -1,4 +1,17 @@
 package kuit2.server.common.exception;
 
+import kuit2.server.common.response.status.ResponseStatus;
+
 public class OrderException extends RuntimeException {
+    private final ResponseStatus exceptionStatus;
+
+    public OrderException(ResponseStatus exceptionStatus) {
+        super(exceptionStatus.getMessage());
+        this.exceptionStatus = exceptionStatus;
+    }
+
+    public OrderException(ResponseStatus exceptionStatus, String message) {
+        super(message);
+        this.exceptionStatus = exceptionStatus;
+    }
 }
--- src/main/java/kuit2/server/service/OrderService.java
+++ src/main/java/kuit2/server/service/OrderService.java
@@ -1,6 +1,8 @@
 package kuit2.server.service;
 
 import kuit2.server.dto.order.PostOrderRequest;
+import kuit2.server.dto.order.PostOrderResponse;
+import kuit2.server.dto.user.PostUserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    public Object order(PostOrderRequest postOrderRequest) {
-
+    public PostOrderResponse order(PostOrderRequest postOrderRequest) {
+        return new PostOrderResponse(1, "주문하기 test입니다.");
     }
 }
