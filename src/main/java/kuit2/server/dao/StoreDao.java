ddd83141e7c86650977b40649f06fe31b05eb502
lisiant <wkdxod321@gmail.com>
2023-11-30 18:20:43 +0900

    StoreController 수정, API 구현

--- src/main/java/kuit2/server/dao/StoreDao.java
+++ src/main/java/kuit2/server/dao/StoreDao.java
@@ -1,5 +1,6 @@
 package kuit2.server.dao;
 
+import kuit2.server.dto.store.GetStoreResponse;
 import kuit2.server.dto.store.PostStoreRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -10,6 +11,8 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -22,11 +25,52 @@ public class StoreDao {
         this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
-    public long createStore(PostStoreRequest postStoreRequest){
+    public long createStore(PostStoreRequest postStoreRequest) {
         String sql = "insert into store(name, category, address, phone, content) VALUES (:name, :category, :address, :phone, :content)";
         SqlParameterSource param = new BeanPropertySqlParameterSource(postStoreRequest);
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(sql, param, keyHolder);
         return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public List<GetStoreResponse> getStoresByName(String name, String status) {
+        String sql = "select name, category, address, phone, content from store where name like :name and status=:status";
+        Map<String, Object> param = Map.of(
+                "name", "%" + name + "%",
+                "status", status);
+
+        return jdbcTemplate.query(sql, param, ((rs, rowNum) ->
+                new GetStoreResponse(
+                        rs.getString("name"),
+                        rs.getString("category"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("content")
+                ))
+        );
+    }
+
+    public List<GetStoreResponse> getStoresByCategory(String category, String status){
+        String sql = "select name, category, address, phone, content from store where category=:category and status=:status";
+        Map<String, Object> param = Map.of(
+                "category", category,
+                "status", status
+        );
+        return jdbcTemplate.query(sql,param, ((rs, rowNum) ->
+                new GetStoreResponse(
+                        rs.getString("name"),
+                        rs.getString("category"),
+                        rs.getString("address"),
+                        rs.getString("phone"),
+                        rs.getString("content")
+                )));
+    }
+
+    public int modifyStoreStatus_deleted(long storeId) {
+        String sql = "update store set status=:status where store_id=:store_id";
+        Map<String, Object> param = Map.of(
+                "status", "deleted",
+                "store_id", storeId);
+        return jdbcTemplate.update(sql, param);
     }
 }
--- src/main/java/kuit2/server/service/StoreService.java
+++ src/main/java/kuit2/server/service/StoreService.java
@@ -1,11 +1,17 @@
 package kuit2.server.service;
 
+import kuit2.server.common.exception.DatabaseException;
 import kuit2.server.dao.StoreDao;
+import kuit2.server.dto.store.GetStoreResponse;
 import kuit2.server.dto.store.PostStoreRequest;
 import kuit2.server.dto.store.PostStoreResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static kuit2.server.common.response.status.BaseExceptionResponseStatus.DATABASE_ERROR;
 
 @Slf4j
 @Service
@@ -18,5 +24,24 @@ public class StoreService {
         log.info("[StoreService.createStore]");
         long storeId = storeDao.createStore(postStoreRequest);
         return new PostStoreResponse(storeId);
+    }
+
+    public List<GetStoreResponse> getStoresByName(String name, String status) {
+        log.info("[StoreService.getStoresByName]");
+        return storeDao.getStoresByName(name, status);
+    }
+
+    public List<GetStoreResponse> getStoresByCategory(String category, String status){
+        log.info("[StoreService.getStoresByCategory]");
+        return storeDao.getStoresByCategory(category, status);
+    }
+
+    public void modifyStoreStatus_deleted(long storeId) {
+        log.info("StoreService.modifyStoreStatus_deleted]");
+
+        int affectedRows = storeDao.modifyStoreStatus_deleted(storeId);
+        if (affectedRows != 1) {
+            throw new DatabaseException(DATABASE_ERROR);
+        }
     }
 }
