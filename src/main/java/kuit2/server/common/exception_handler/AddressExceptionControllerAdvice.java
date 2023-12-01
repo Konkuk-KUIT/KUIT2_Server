e61bb2d404701328fa2a25b0f5e4f23fa16a9586
parkjiwon <ji11won@naver.com>
2023-12-01 14:25:10 +0900

    Add : RestControllerAdvice 추가

---
+++ src/main/java/kuit2/server/common/exception_handler/AddressExceptionControllerAdvice.java
@@ -0,0 +1,11 @@
+package kuit2.server.common.exception_handler;
+
+import jakarta.annotation.Priority;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Priority(0)
+@RestControllerAdvice
+public class AddressExceptionControllerAdvice {
+}
